e493127ecbce72970a4d2553aeaa28026cc2a216
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2023-08-18 06:54:11 -0700

    update docstrings and sphinx config (#1)

--- src/biocborn/_checks.py
+++ src/biocborn/_checks.py
@@ -6,14 +6,16 @@ __license__ = "MIT"
 
 
 def is_list_of_type(x: Any, target_type: Callable) -> bool:
-    """Checks if `x` is a list of `target_type`.
+    """Checks if ``x`` is a list, and whether all elements of the list
+    are of the same type.
 
     Args:
-        x (Any): Any object.
-        target_type (Callable): Type to check for, e.g. str, int.
+        x (Any): Any list.
+        target_type (callable): Type to check for,
+            e.g. :py:class:`str`, :py:class:`int`.
 
     Returns:
-        bool: True if 'x' is list and all values are of the same type.
+        bool: True if ``x`` satisfies the conditions.
     """
     return (isinstance(x, list) or isinstance(x, tuple)) and all(
         isinstance(item, target_type) for item in x
